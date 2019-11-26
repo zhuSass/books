a4987f126d8bea55717b347d4a5b12b9a86838f4
zhuzengzhi <258953304@qq.com>
2019-11-26 18:08:50 +0800

    添加文章详情页面

--- src/page/other/reading/css.tsx
+++ src/page/other/reading/css.tsx
@@ -2,7 +2,39 @@ import { StyleSheet, } from "react-native";
 
 export default StyleSheet.create({
     homeWrap: {
-
+        position: 'relative',
+    },
+    bgColorContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: -1,
+    },
+    bgColorContainerTop: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: 142,
+        resizeMode: 'stretch',
+    },
+    bgColorContainerBottom: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: 142,
+        resizeMode: 'stretch',
+    },
+    bgColorContainerMain: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'stretch',
+    },
+    readingMainContainer: {
+        paddingTop: 10,
     },
     headerRight: {
         display: 'flex',
@@ -31,5 +63,22 @@ export default StyleSheet.create({
     leaveMessageIcon: {
         fontSize: 20,
         color: '#4D4D4D',
+    },
+    ReadingMainItem: {
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+    ReadingMainItemTitle: {
+        textAlign: 'center',
+        color: '#262626',
+        fontSize: 27,
+        marginTop: 27,
+        marginBottom: 27,
+        fontWeight: '400',
+    },
+    ReadingMainItemcontent: {
+        fontSize: 18,
+        lineHeight: 32.4,
+        color: '#262626',
     },
 });
